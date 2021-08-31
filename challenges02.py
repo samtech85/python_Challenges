10e9a887fe46882825aa42258724ab7718f7ad0d
samTech85 <sbonheur85@gmail.com>
2021-08-30 22:04:31 -0400

    finishing with challenges02

---
+++ challenges02.py
@@ -0,0 +1,96 @@
+# ############################################################
+# 012
+'''
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+if num1 > num2:
+    print(num2,num1)
+else:
+    print(num1,num2)
+'''
+
+
+# ############################################################
+# 013
+'''num = int(input("Enter a value less than 20:"))
+if num >= 20:
+    print("Too high")
+else:
+    print("Thank you :)")'''
+
+
+# ############################################################
+# 014
+'''num = int(input("Enter a value between 10 and 20:"))
+
+if num >= 10 and num <= 20:
+    print("Thank you")
+else:
+    print("Incorrect answer :(")'''
+
+
+
+# ############################################################
+# 015
+
+'''colour = input("Type in your favourite colour:")
+if colour == "red" or colour == "RED" or colour == "Red":
+    print("I like red too.")
+else:
+    print("I don't like that colour, I prefer red")'''
+
+
+# ############################################################
+# 016
+
+'''raining = input("Is it raining? \n:")
+raining = str.lower(raining)
+
+if raining == "yes":
+    windy = input("Is it windy? \n:")
+    windy = str.lower(windy)
+    if windy == "yes":
+        print("It is too windy for an umbrella")
+    else:
+        print("Take an umbrella")
+else:
+    print("Enjoy your day!")'''
+
+
+
+# ############################################################
+# 017
+'''age = int(input("What is your age? \n:"))
+if age >= 18:
+    print("You can vote")
+elif age == 17:
+    print("You can learn to drive")
+elif age == 16:
+    print("You can buy a lottery ticket")
+else:
+    print('You can go Trick-or-Treating')'''
+
+
+
+# ############################################################
+# 018
+
+'''num = int(input("Enter a number \n:"))
+if num < 10:
+    print("Too low")
+elif num >= 10 and num <= 20:
+    print("Correct")
+else:
+    print("Too high")'''
+# ############################################################
+# 019
+'''num = input("Enter 1, 2 or 3:")
+if num == "1":
+    print("Thank you")
+elif num == "2":
+    print("Well done")
+elif num == "3":
+    print("Correct")
+else:
+    print("Error message")'''
