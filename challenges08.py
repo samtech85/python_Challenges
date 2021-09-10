bade724f735e6911d262217ccf328d9b793444c4
samTech85 <sbonheur85@gmail.com>
2021-09-10 18:34:48 -0400

    Taking a break at challenges 065

---
+++ challenges08.py
@@ -0,0 +1,132 @@
+from turtle import *
+import turtle
+
+# turtle
+
+# ####################################################################
+# 060
+'''
+turtle.shape("turtle")
+for i in range(0,4):
+    turtle.forward(100)
+    turtle.right(90)
+
+turtle.exitonclick()'''
+
+# ####################################################################
+# 061
+'''
+turtle.shape("turtle")
+for i in range(0,3):
+    turtle.forward(120)
+    turtle.right(120)
+turtle.exitonclick()
+'''
+# ####################################################################
+# 062
+'''
+turtle.hideturtle()
+for i in range(0, 360):
+    turtle.forward(2)
+    turtle.right(1)
+turtle.exitonclick()
+'''
+
+
+# ####################################################################
+# 063
+
+'''turtle.color("black", "green")
+turtle.begin_fill()
+for i in range(0,4):
+    turtle.forward(70)
+    turtle.right(90)
+
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+
+turtle.pendown()
+turtle.color("black", "yellow")
+turtle.begin_fill()
+for i in range(0, 4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+
+turtle.pendown()
+turtle.color("black", "blue")
+turtle.begin_fill()
+for i in range(0, 4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.end_fill()
+
+turtle.exitonclick()
+'''
+
+# #####################################################################
+# 064
+
+'''for i in range(0,5):
+    turtle.forward(100)
+    turtle.right(144)
+turtle.exitonclick()'''
+
+# #####################################################################
+# 065
+'''
+# One
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+
+#  Two
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+
+turtle.forward(75)
+turtle.right(90)
+turtle.forward(50)
+
+turtle.right(90)
+turtle.forward(75)
+turtle.left(90)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(75)
+
+# three
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(75)
+turtle.left(180)
+
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(50)
+
+turtle.left(90)
+turtle.forward(75)
+turtle.hideturtle()
+
+turtle.exitonclick()
+'''
+
+
+
+# a = 105
+# b = int(input("ameriken: "))
+# c = a * b/ 5
+# print(int(c), "dola ayisyen.")
